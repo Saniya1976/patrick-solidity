a30a7b8fab6ac53a4879a4e63d6732d8158b62b6
Saniya1976 <saniyakumari1976@gmail.com>
2025-10-23 15:45:45 +0530

    Family Wallet contract practice

---
+++ contracts/FamilyWallet.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract FamilyWallet{
+
+   address public owner;
+   mapping(address=>uint256) public allowance;
+    constructor(){
+        owner=msg.sender;
+    }
+   modifier onlyOwner(){
+          require( msg.sender==owner,"U are not the Owner");
+          _;
+   }
+   modifier memberOnly(){
+    require(allowance[msg.sender]>0,"Not a member");
+    _;
+   }
+    function deposit(address _member,uint256 _amount) external payable{
+        require(msg.value>0,"amount to be deposited cannot be 0");
+        allowance[_member]+=_amount;
+    }
+    function withdraw(uint256 _amount) external memberOnly(){
+        require(_amount<=allowance[msg.sender],"Exceeds allowance");
+        require(_amount <= address(this).balance, "Insufficient contract balance");
+        allowance[msg.sender]-=_amount;
+        payable(msg.sender).transfer(_amount);
+
+    }
+    function removeMember(address _member) external onlyOwner(){
+            allowance[_member]=0;
+    }
+    function addMember() external onlyOwner(){
+        allowance[msg.sender]+=1;
+    }
+    function setAllowance(address _member,uint256 _amount)external onlyOwner(){
+        allowance[_member]=_amount;
+    }
+
+    function ownerWithdraw(uint256 _amount)external onlyOwner(){
+        require(_amount<=address(this).balance,"Not Sufficient Balance");
+        payable(owner).transfer(_amount);
+    }
+    function checkMyBalance() external view returns(uint256){
+        return allowance[msg.sender];
+    }
+     function getContractBalance() external view returns(uint256){
+        return address(this).balance;
+    }
+}
+
