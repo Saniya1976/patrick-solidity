4620e063aed00f082e8adb5042406dc9e3ac3ba0
Saniya1976 <saniyakumari1976@gmail.com>
2025-10-23 12:57:24 +0530

    optimized SimpleBank contract

--- contracts/BankContract.sol
+++ contracts/BankContract.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.30;
 contract SimpleBank{
     mapping(address=>uint256) private balances;
 
-    function withdraw(uint256 amount) public {
+    function withdraw(uint256 amount)external {
         require(amount<= balances[msg.sender],"not enough balance in account to be withdrawn");
         balances[msg.sender]-=amount;
         payable(msg.sender).transfer(amount);
     }
-    function Deposit() public payable{
+    function deposit()external payable{
         require(msg.value>0,"Deposit some valid amount");
         balances[msg.sender]+=msg.value;
     }
-    function CheckBal() public view returns(uint256){
+    function checkBal() public view returns(uint256){
         return balances[msg.sender];
     }
-    function TotalBalance() public view returns(uint256){
+    function totalBalance() public view returns(uint256){
         return address(this).balance;
     }
 }
